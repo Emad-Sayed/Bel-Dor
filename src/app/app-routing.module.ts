968b74f4278f843bdb836433b2d9f675a651f9bb
ahmedamrzezo <ahmedamrzezo@gmail.com>
2020-09-10 16:57:43 +0200

    lazy load about us component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    path: '/home',
+    // loadChildren: () => import('./core/home/home.module').then(m => m.)
+  },
+  {
+    path: '/about-us',
+    loadChildren: () => import('./core/about-us/about-us.module').then(m => m.AboutUsModule)
+  },
+  {
+    path: '',
+    // component:
+  },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
