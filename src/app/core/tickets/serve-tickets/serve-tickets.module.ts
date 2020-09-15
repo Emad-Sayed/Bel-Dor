edae962275f12e13df1ca53da1c22adea62d9fc2
ahmedamrzezo <ahmedamrzezo@gmail.com>
2020-09-15 12:22:13 -0700

    lazy loading module

--- src/app/core/tickets/serve-tickets/serve-tickets.module.ts
+++ src/app/core/tickets/serve-tickets/serve-tickets.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { TicketsModule } from '../tickets.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    TicketsModule
   ]
 })
 export class ServeTicketsModule { }
