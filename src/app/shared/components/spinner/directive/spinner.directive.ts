fd9a9018db6ac24cfc9bc4f3f9a0feb894010d3c
ahmedamrzezo <ahmedamrzezo@gmail.com>
2020-09-19 00:56:00 +0200

    spinner directive to inject spinner comp

--- src/app/shared/components/spinner/directive/spinner.directive.ts
+++ src/app/shared/components/spinner/directive/spinner.directive.ts
@@ -1,9 +1,20 @@
 import { Directive, ViewContainerRef } from '@angular/core';
+import { SpinnerService } from '../service/spinner.service';
 
 @Directive({
-  selector: '[appSpinnerPlaceholder]'
+  selector: '[appSpinnerPlaceholder]',
 })
 export class SpinnerDirective {
-  constructor(public viewContainerRef: ViewContainerRef) { }
+  
+  constructor(
+    public viewContainerRef: ViewContainerRef,
+    private _spinnerService: SpinnerService
+  ) {
+  }
 
+  sendViewContainer() {
+    return this._spinnerService.spinnerViewContainerSubject.next(
+      this.viewContainerRef
+    );
+  }
 }
