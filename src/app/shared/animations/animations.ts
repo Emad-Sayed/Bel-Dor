8b002b10efc7627e923dd8d328aa4b6cf399bf8c
ahmedamrzezo <ahmedamrzezo@gmail.com>
2020-09-19 19:44:18 +0200

    home ticket smaller

--- src/app/shared/animations/animations.ts
+++ src/app/shared/animations/animations.ts
@@ -3,7 +3,7 @@ import { trigger, transition, style, animate, state } from '@angular/animations'
 export const ticketAnimation = 
   trigger('ticketAnim', [
     state('void', style({right: '-40vw', bottom: '-40vh'})),
-    state('*', style({right: '-150px', bottom: '-40px'})),
+    state('*', style({right: '-100px', bottom: '-50px'})),
     transition('void => *', [
       animate('600ms 500ms cubic-bezier(0.55, 0.06, 0.68, 0.19)')
     ])
