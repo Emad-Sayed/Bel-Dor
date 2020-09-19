f9387e716977a865eae08d7e1bdc2de45e5b112e
ahmedamrzezo <ahmedamrzezo@gmail.com>
2020-09-19 19:54:41 +0200

    ticket number input

--- src/app/shared/components/ticket-graphic/ticket-graphic.component.ts
+++ src/app/shared/components/ticket-graphic/ticket-graphic.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ticket-graphic',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket-graphic.component.scss']
 })
 export class TicketGraphicComponent implements OnInit {
+
+  @Input() ticketNumber: number;
 
   constructor() { }
 
