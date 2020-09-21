03d67543bd20b121bb23e6a501ba3de12061004d
ahmedamrzezo <ahmedamrzezo@gmail.com>
2020-09-21 14:54:49 -0700

    history comp route

--- src/app/core/tickets/visitor-tickets/visitor-tickets-routing.module.ts
+++ src/app/core/tickets/visitor-tickets/visitor-tickets-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GenerateTicketComponent } from './generate-ticket/generate-ticket.component';
+import { TicketHistoryComponent } from './ticket-history/ticket-history.component';
 import { VisitorTicketsComponent } from './visitor-tickets.component';
 
 const routes: Routes = [
@@ -13,8 +14,8 @@ const routes: Routes = [
     component: GenerateTicketComponent
   },
   {
-    path: 'generate',
-    component: GenerateTicketComponent
+    path: 'history',
+    component: TicketHistoryComponent
   }
 ];
 
