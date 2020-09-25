98c9a20052112d7598c236df6acd05e3147c57b3
ahmedamrzezo <ahmedamrzezo@gmail.com>
2020-09-25 18:08:17 +0200

    closed ticket logic and stlyes

--- src/app/core/tickets/visitor-tickets/ticket-history/ticket-history.component.ts
+++ src/app/core/tickets/visitor-tickets/ticket-history/ticket-history.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SpinnerDirective } from 'src/app/shared/components/spinner/directive/spinner.directive';
+import { TranslationsService } from 'src/app/shared/services/translations.service';
+import { VisitorTicketsService } from '../visitor-tickets.service';
 
 @Component({
   selector: 'app-ticket-history',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket-history.component.scss']
 })
 export class TicketHistoryComponent implements OnInit {
+  @ViewChild(SpinnerDirective, {static: true, read: SpinnerDirective}) spinnerPlaceholder: SpinnerDirective;
 
-  constructor() { }
+  slideConfig = {
+    arrows: true,
+    dot: false,
+    infinite: false,
+    focusOnSelect: false,
+    slidesToShow: 1, 
+    slidesToScroll: 1,
+    rtl: !this._translationService.isEnglish
+  };
+
+  closedTickets = [];
+
+  constructor(
+    private _visitorTicketsService: VisitorTicketsService,
+    public _translationService: TranslationsService
+    ) { }
 
   ngOnInit(): void {
+    this.spinnerPlaceholder.sendViewContainer();
+
+    const data = {
+      statusIds: [3],
+      pageSize: 100
+    };
+    this._visitorTicketsService.getVisitorTickets(data)
+    .subscribe(res => {
+      this.closedTickets = res['data'];
+    });
   }
 
 }
