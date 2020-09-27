bfed1776496706f853bd01d2a165108c7a25e988
ahmedamrzezo <ahmedamrzezo@gmail.com>
2020-09-27 14:51:11 -0700

    remove imports

--- src/app/core/tickets/visitor-tickets/visitor-tickets-routing.module.ts
+++ src/app/core/tickets/visitor-tickets/visitor-tickets-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GenerateTicketComponent } from './generate-ticket/generate-ticket.component';
-import { MissedTicketsComponent } from './missed-tickets/missed-tickets.component';
-import { TicketHistoryComponent } from './ticket-history/ticket-history.component';
 import { VisitorTicketsComponent } from './visitor-tickets.component';
 
 const routes: Routes = [
