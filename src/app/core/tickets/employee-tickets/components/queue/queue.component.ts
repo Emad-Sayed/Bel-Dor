8feaaea7a5be9aa9e880ffc187903dfd032c9643
ahmedamrzezo <ahmedamrzezo@gmail.com>
2020-10-01 20:46:39 +0200

    logic for serve ticket and close ticket

--- src/app/core/tickets/employee-tickets/components/queue/queue.component.ts
+++ src/app/core/tickets/employee-tickets/components/queue/queue.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { SpinnerDirective } from 'src/app/shared/components/spinner/directive/spinner.directive';
+import { TranslationsService } from 'src/app/shared/services/translations.service';
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-queue',
@@ -6,10 +10,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./queue.component.scss']
 })
 export class QueueComponent implements OnInit {
+  @ViewChild(SpinnerDirective, {static: true, read: SpinnerDirective}) spinnerPlaceholder: SpinnerDirective;
 
-  constructor() { }
+  ticketQueue = [];
+
+  param: Params;
+
+  queueActivated = false;
+
+  constructor(
+    private actRoute: ActivatedRoute,
+    public _translationService: TranslationsService,
+    private _employeeService: EmployeeService
+  ) { }
 
   ngOnInit(): void {
+    this.actRoute.queryParams
+    .subscribe(param => {
+      this.spinnerPlaceholder.sendViewContainer();
+      this.param = param;
+      const data = {
+        statusIds: [1],
+        pageSize: 100
+      }
+      if (this.param.queueType === 'missed') {
+        data['statusIds'] = [4];
+      }
+
+      this.updateQueue(data);
+    });
+  }
+
+  updateQueue(data: {statusIds: any, pageSize: any}) {
+    this._employeeService.getEmployeeDailyTickets(data)
+      .subscribe(res => {
+        console.log(res);
+        this.ticketQueue = res['data'];
+      });
+  }
+
+  nextInQueue() {
+    this.queueActivated = true;
+
+    this._employeeService.serveTicket()
+    .subscribe();
+  }
+
+  removeFromQueue() {
+    this.queueActivated = false;
+
+    this._employeeService.closeServedTicket()
+    .subscribe(() => {
+      this.updateQueue({statusIds: [1], pageSize: 100})
+    });
   }
 
 }
