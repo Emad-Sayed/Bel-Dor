3f4c97a4ad25bd670821d22b551aa1d2ea07c02e
ahmedamrzezo <ahmedamrzezo@gmail.com>
2020-10-01 20:46:59 +0200

    method for calling api for serve and close tickets

--- src/app/core/tickets/employee-tickets/services/employee.service.ts
+++ src/app/core/tickets/employee-tickets/services/employee.service.ts
@@ -1,7 +1,25 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 
 export class EmployeeService {
 
+  constructor(private http$: HttpClient) { }
+
+  getEmployeeDailyTickets(data) {
+    let params = new HttpParams();
+    params = params.set('statusIds', data.statusIds);
+    params = params.set('pageSize', data.pageSize);
+    
+    return this.http$.get(`${environment.baseUrl}Ticket/EmployeeDailyTickets`, {params: params});
+  }
+  
+  serveTicket() {
+    return this.http$.post(`${environment.baseUrl}Ticket/ServeTicket`, null);
+  }
+  closeServedTicket() {
+    return this.http$.post(`${environment.baseUrl}Ticket/CloseServedTicket`, null);
+  }
 }
